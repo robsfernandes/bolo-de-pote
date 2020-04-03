4891211d8dc83280c464ab9c9aa8f13fb02f9165
robsfernandes <62964827+robsfernandes@users.noreply.github.com>
2020-04-02 21:22:12 -0300

    contador

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,36 @@
 jQuery.noConflict();
 jQuery(document).ready(function($){
 	
+	// Set the date we're counting down to
+  var countDownDate = new moment().add(.75, 'hours');
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("contador").innerHTML = hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("contador").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
 	"use strict";	
 	
 	var isSafari = navigator.userAgent.match(/Safari/i) ? true : false;
